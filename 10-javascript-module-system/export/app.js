33093fe3137824e678c119fc08681e15d89c5f76
ferenc-attila <attila.ferenc.80@gmail.com>
2022-08-28 18:00:18 +0200

    import all

--- 10-javascript-module-system/export/app.js
+++ 10-javascript-module-system/export/app.js
@@ -1,10 +1,15 @@
 'use strict';
 
-import {
-    sum as sumValues, subtract as subtractValues, calculateCircleArea //más néven is lehet importálni
-} from './module.js';
+// Importálás egyes függvényeket
+// import {
+//     sum as sumValues, subtract as subtractValues, calculateCircleArea //más néven is lehet importálni
+// } from './module.js';
 
-console.log(sumValues(45,8));
-console.log(subtractValues(45,8));
-console.log(calculateCircleArea(15));
+//Importálás mindent egy fájlból
+
+import * as MyMath from './module.js'; //ekkor egy MyMath objektumként importálja a fájl tartalmát
+
+console.log(MyMath.sumValues(45,8));
+console.log(MyMath.subtractValues(45,8));
+console.log(MyMath.calculateCircleArea(15));
 // console.log(pi); Hibára fut, de a függvényben használható
