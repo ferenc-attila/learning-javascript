61ab34914c67cf6574cc9da4076cb8da571ac852
ferenc-attila <attila.ferenc.80@gmail.com>
2022-08-28 17:35:26 +0200

    export 1

---
+++ 10-javascript-module-system/export/app.js
@@ -0,0 +1,8 @@
+'use strict';
+
+import {
+    sumValues, subtractValues
+} from './module.js';
+
+console.log(sumValues(45,8));
+console.log(subtractValues(45,8));
---
+++ 10-javascript-module-system/export/module.js
@@ -0,0 +1,5 @@
+'use strict';
+
+export const sumValues = (x,y) => x + y;
+
+export const subtractValues = (x,y) => x - y;
